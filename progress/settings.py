6a0a0058cf2a92200be35623a3630b8fd54f3610
kensolfar <kensol23@gmail.com>
2024-06-16 15:06:40 -0600

    database

--- progress/settings.py
+++ progress/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'progress.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+        "NAME": "progressdb",
     }
 }
 
